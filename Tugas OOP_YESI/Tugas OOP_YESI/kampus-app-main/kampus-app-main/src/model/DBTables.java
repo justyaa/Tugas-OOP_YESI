460cfd94c639150659698a99cba745a7861bc1c6
justyaa <147975228+justyaa@users.noreply.github.com>
2023-10-30 22:33:49 +0700

    Delete Tugas OOP_YESI directory

--- Tugas OOP_YESI/Tugas OOP_YESI/kampus-app-main/kampus-app-main/src/model/DBTables.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-import java.util.ArrayList;
-
-public class DBTables {
-    public ArrayList<Mahasiswa> dataMahasiswa = new ArrayList<>();
-    public ArrayList<Prodi> dataProdi = new ArrayList<>();
-}
